94dd925f866053acb84ba11a12775751a317e055
jaytea33 <yerffej@gmail.com>
2022-04-22 22:55:11 -0700

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,371 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.mountedrootbtn = New System.Windows.Forms.Button()
+        Me.oscdimgbtn = New System.Windows.Forms.Button()
+        Me.oscdimgtext = New System.Windows.Forms.TextBox()
+        Me.mountedroottext = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.efisysbintext = New System.Windows.Forms.TextBox()
+        Me.efisysbinbtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.destinationtext = New System.Windows.Forms.TextBox()
+        Me.destinationbtn = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.makeisobtn = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.etfsbootcomtext = New System.Windows.Forms.TextBox()
+        Me.etfsbootcombtn = New System.Windows.Forms.Button()
+        Me.finaloutputtext = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(640, 511)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(300, 96)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'mountedrootbtn
+        '
+        Me.mountedrootbtn.Location = New System.Drawing.Point(659, 89)
+        Me.mountedrootbtn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mountedrootbtn.Name = "mountedrootbtn"
+        Me.mountedrootbtn.Size = New System.Drawing.Size(162, 41)
+        Me.mountedrootbtn.TabIndex = 2
+        Me.mountedrootbtn.Text = "Browse"
+        Me.mountedrootbtn.UseVisualStyleBackColor = True
+        '
+        'oscdimgbtn
+        '
+        Me.oscdimgbtn.Location = New System.Drawing.Point(658, 30)
+        Me.oscdimgbtn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.oscdimgbtn.Name = "oscdimgbtn"
+        Me.oscdimgbtn.Size = New System.Drawing.Size(162, 42)
+        Me.oscdimgbtn.TabIndex = 3
+        Me.oscdimgbtn.Text = "Browse"
+        Me.oscdimgbtn.UseVisualStyleBackColor = True
+        '
+        'oscdimgtext
+        '
+        Me.oscdimgtext.AcceptsTab = True
+        Me.oscdimgtext.Location = New System.Drawing.Point(128, 31)
+        Me.oscdimgtext.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.oscdimgtext.Name = "oscdimgtext"
+        Me.oscdimgtext.Size = New System.Drawing.Size(523, 26)
+        Me.oscdimgtext.TabIndex = 4
+        '
+        'mountedroottext
+        '
+        Me.mountedroottext.Location = New System.Drawing.Point(128, 96)
+        Me.mountedroottext.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mountedroottext.Name = "mountedroottext"
+        Me.mountedroottext.Size = New System.Drawing.Size(523, 26)
+        Me.mountedroottext.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(128, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Path to oscdimg.exe"
+        '
+        'efisysbintext
+        '
+        Me.efisysbintext.Location = New System.Drawing.Point(126, 239)
+        Me.efisysbintext.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.efisysbintext.Name = "efisysbintext"
+        Me.efisysbintext.Size = New System.Drawing.Size(523, 26)
+        Me.efisysbintext.TabIndex = 8
+        '
+        'efisysbinbtn
+        '
+        Me.efisysbinbtn.Location = New System.Drawing.Point(659, 223)
+        Me.efisysbinbtn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.efisysbinbtn.Name = "efisysbinbtn"
+        Me.efisysbinbtn.Size = New System.Drawing.Size(162, 42)
+        Me.efisysbinbtn.TabIndex = 7
+        Me.efisysbinbtn.Text = "Browse"
+        Me.efisysbinbtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(124, 215)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(520, 20)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Path to efisys.bin on USB (*USB*\EFI\MICROSOFT\BOOT\EFISYS.BIN)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(128, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(209, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Select letter of USB (i.e. G:\)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(122, 296)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(336, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Enter Path where generated ISO will be stored"
+        '
+        'destinationtext
+        '
+        Me.destinationtext.Location = New System.Drawing.Point(126, 320)
+        Me.destinationtext.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.destinationtext.Name = "destinationtext"
+        Me.destinationtext.Size = New System.Drawing.Size(523, 26)
+        Me.destinationtext.TabIndex = 12
+        '
+        'destinationbtn
+        '
+        Me.destinationbtn.Location = New System.Drawing.Point(658, 319)
+        Me.destinationbtn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.destinationbtn.Name = "destinationbtn"
+        Me.destinationbtn.Size = New System.Drawing.Size(162, 42)
+        Me.destinationbtn.TabIndex = 11
+        Me.destinationbtn.Text = "Save"
+        Me.destinationbtn.UseVisualStyleBackColor = True
+        '
+        'makeisobtn
+        '
+        Me.makeisobtn.Location = New System.Drawing.Point(126, 511)
+        Me.makeisobtn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.makeisobtn.Name = "makeisobtn"
+        Me.makeisobtn.Size = New System.Drawing.Size(294, 96)
+        Me.makeisobtn.TabIndex = 14
+        Me.makeisobtn.Text = "Make ISO"
+        Me.makeisobtn.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(128, 135)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(454, 20)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Path to etfsboot.com on USB (*USB*\BOOT\ETFSBOOT.COM)"
+        '
+        'etfsbootcomtext
+        '
+        Me.etfsbootcomtext.Location = New System.Drawing.Point(128, 161)
+        Me.etfsbootcomtext.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.etfsbootcomtext.Name = "etfsbootcomtext"
+        Me.etfsbootcomtext.Size = New System.Drawing.Size(522, 26)
+        Me.etfsbootcomtext.TabIndex = 16
+        '
+        'etfsbootcombtn
+        '
+        Me.etfsbootcombtn.Location = New System.Drawing.Point(658, 156)
+        Me.etfsbootcombtn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.etfsbootcombtn.Name = "etfsbootcombtn"
+        Me.etfsbootcombtn.Size = New System.Drawing.Size(162, 42)
+        Me.etfsbootcombtn.TabIndex = 15
+        Me.etfsbootcombtn.Text = "Browse"
+        Me.etfsbootcombtn.UseVisualStyleBackColor = True
+        '
+        'finaloutputtext
+        '
+        Me.finaloutputtext.Location = New System.Drawing.Point(126, 404)
+        Me.finaloutputtext.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.finaloutputtext.Name = "finaloutputtext"
+        Me.finaloutputtext.Size = New System.Drawing.Size(525, 26)
+        Me.finaloutputtext.TabIndex = 19
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(123, 379)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(166, 21)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Final CMD Output"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(828, 385)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(162, 52)
+        Me.Button3.TabIndex = 20
+        Me.Button3.Text = "Show Full Command"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(659, 385)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(162, 52)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Copy to Clipboard"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoCheck = False
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(850, 40)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(106, 24)
+        Me.CheckBox1.TabIndex = 22
+        Me.CheckBox1.Text = "File Exists"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoCheck = False
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(850, 163)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(106, 24)
+        Me.CheckBox2.TabIndex = 23
+        Me.CheckBox2.Text = "File Exists"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoCheck = False
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(850, 233)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(106, 24)
+        Me.CheckBox3.TabIndex = 24
+        Me.CheckBox3.Text = "File Exists"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 21)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(108, 91)
+        Me.Button4.TabIndex = 25
+        Me.Button4.Text = "Download ADK"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 156)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(106, 101)
+        Me.Button5.TabIndex = 26
+        Me.Button5.Text = "Install oscdimg"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1041, 728)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.finaloutputtext)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.etfsbootcomtext)
+        Me.Controls.Add(Me.etfsbootcombtn)
+        Me.Controls.Add(Me.makeisobtn)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.destinationtext)
+        Me.Controls.Add(Me.destinationbtn)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.efisysbintext)
+        Me.Controls.Add(Me.efisysbinbtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.mountedroottext)
+        Me.Controls.Add(Me.oscdimgtext)
+        Me.Controls.Add(Me.oscdimgbtn)
+        Me.Controls.Add(Me.mountedrootbtn)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "WinPE USB to ISO Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents mountedrootbtn As Button
+    Friend WithEvents oscdimgbtn As Button
+    Friend WithEvents oscdimgtext As TextBox
+    Friend WithEvents mountedroottext As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents efisysbintext As TextBox
+    Friend WithEvents efisysbinbtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents destinationtext As TextBox
+    Friend WithEvents destinationbtn As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents makeisobtn As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents etfsbootcomtext As TextBox
+    Friend WithEvents etfsbootcombtn As Button
+    Friend WithEvents finaloutputtext As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,244 @@
+﻿Public Class Form1
+    
+    Private Sub FolderBrowserDialog1_HelpRequest(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Computer.FileSystem.FileExists("C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe") Then
+            'MessageBox.Show("Default oscdimg directory found!")
+            oscdimgtext.Text = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe"
+            finaloutputtext.Text = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe"
+            CheckBox1.Checked = True
+        Else
+            CheckBox1.Checked = False
+        End If
+
+        If oscdimgtext.Text.Contains(" ") Then
+            oscdimgtext.Text = """" & oscdimgtext.Text & """"
+            finaloutputtext.Text = """" & finaloutputtext.Text & """"
+        End If
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk_1(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles mountedrootbtn.Click
+        Dim fld As New FolderBrowserDialog()
+        If fld.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            mountedroottext.Text = fld.SelectedPath
+            etfsbootcomtext.Text = fld.SelectedPath + "BOOT\etfsboot.com"
+            efisysbintext.Text = fld.SelectedPath + "EFI\MICROSOFT\BOOT\efisys.bin"
+            finaloutputtext.Text = fld.SelectedPath
+        End If
+
+        If My.Computer.FileSystem.FileExists(fld.SelectedPath + "BOOT\etfsboot.com") Then
+            CheckBox2.Checked = True
+        Else
+            CheckBox2.Checked = False
+        End If
+
+        If My.Computer.FileSystem.FileExists(fld.SelectedPath + "EFI\MICROSOFT\BOOT\efisys.bin") Then
+            CheckBox3.Checked = True
+        Else
+            CheckBox3.Checked = False
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles oscdimgbtn.Click
+        Dim oscdimgvar As String
+        oscdimgvar = oscdimgtext.Text
+        Dim ofd As New OpenFileDialog()
+        ofd.Filter = "OSCDIMG (*oscdimg.exe*)|*oscdimg.exe"
+        ofd.InitialDirectory = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg"
+        OpenFileDialog1.Filter = "COM Files (*.com*)|*.com"
+        If ofd.ShowDialog() = DialogResult.OK Then
+            oscdimgtext.Text = ofd.FileName
+            oscdimgvar = oscdimgtext.Text
+            finaloutputtext.Text = ofd.FileName
+        End If
+
+        If oscdimgtext.Text.Contains(" ") Then
+            oscdimgtext.Text = """" & oscdimgtext.Text & """"
+            finaloutputtext.Text = ofd.FileName
+        End If
+
+        If My.Computer.FileSystem.FileExists(oscdimgvar) Then
+            CheckBox1.Checked = True
+        Else
+            CheckBox1.Checked = False
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles oscdimgtext.TextChanged
+        
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles mountedroottext.TextChanged
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles efisysbinbtn.Click
+        Dim ofd As New OpenFileDialog()
+        ofd.InitialDirectory = ("::{20D04FE0-3AEA-1069-A2D8-08002B30309D}")
+        ofd.Filter = "EFISYS.BIN File (EFISYS.BIN)|EFISYS.BIN"
+        If ofd.ShowDialog() = DialogResult.OK Then
+            efisysbintext.Text = ofd.FileName
+            finaloutputtext.Text = ofd.FileName
+        End If
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles efisysbintext.TextChanged
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles destinationbtn.Click
+        Dim isodestination As String
+        isodestination = destinationtext.Text
+        Dim sfd As New SaveFileDialog()
+        sfd.Filter = "ISO Files (*.iso*)|*.iso"
+        sfd.FileName = "WinPE"
+        sfd.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        If sfd.ShowDialog() = DialogResult.OK Then
+            destinationtext.Text = sfd.FileName
+            isodestination = destinationtext.Text
+            finaloutputtext.Text = sfd.FileName
+        End If
+
+        If destinationtext.Text.Contains(" ") Then
+            destinationtext.Text = """" & destinationtext.Text & """"
+            finaloutputtext.Text = sfd.FileName
+        End If
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles destinationtext.TextChanged
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles makeisobtn.Click
+        Dim finaloutput As String
+        finaloutput = finaloutputtext.Text
+        Process.Start("cmd", "/k " + finaloutput)
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles etfsbootcombtn.Click
+        Dim ofd As New OpenFileDialog()
+        ofd.InitialDirectory = ("::{20D04FE0-3AEA-1069-A2D8-08002B30309D}")
+        ofd.Filter = "ETFSBOOT.COM File (ETFSBOOT.COM)|ETFSBOOT.COM"
+        If ofd.ShowDialog() = DialogResult.OK Then
+            etfsbootcomtext.Text = ofd.FileName
+            finaloutputtext.Text = ofd.FileName
+        End If
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles etfsbootcomtext.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles finaloutputtext.TextChanged
+        Dim oscdimgvar As String
+        Dim etfsbootvar As String
+        Dim efisysvar As String
+        Dim mountedroot As String
+        Dim isodestination As String
+        Dim finaloutput As String
+        Dim quote As String
+
+        oscdimgvar = oscdimgtext.Text
+        etfsbootvar = etfsbootcomtext.Text
+        efisysvar = efisysbintext.Text
+        mountedroot = mountedroottext.Text
+        isodestination = destinationtext.Text
+        finaloutput = finaloutputtext.Text
+        quote = Chr(34)
+
+
+        If oscdimgtext.Text.Contains(" ") And destinationtext.Text.Contains(" ") Then
+            'MessageBox.Show("Save path cannot contain spaces due to syntax limitations. Please choose a directory to save the ISO without spaces")
+            finaloutputtext.Text = quote + oscdimgvar + " " + "-m -o -u2 -udfver102 -bootdata:2#p0,e,b" + etfsbootvar + "#pEF,e,b" + efisysvar + " " + mountedroot + " " + isodestination + quote
+        Else
+
+            finaloutputtext.Text = oscdimgvar + " " + "-m -o -u2 -udfver102 -bootdata:2#p0,e,b" + etfsbootvar + "#pEF,e,b" + efisysvar + " " + mountedroot + " " + isodestination
+        End If
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim finaloutput As String
+        finaloutput = finaloutputtext.Text
+        MessageBox.Show(finaloutput)
+    End Sub
+
+    Private Sub Button2_Click_2(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim finaloutput As String
+        finaloutput = finaloutputtext.Text
+        Clipboard.SetText(finaloutput)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_1(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+        If CheckBox1.Checked = True Then
+            MessageBox.Show("WTF are you doing? You already have the necessary oscdimg file! Quitting due to FUBAR exception")
+            End
+        End If
+
+        If My.Computer.FileSystem.FileExists("C:\adk\adksetup.exe") Then
+            MessageBox.Show("adksetup.exe already exists! Either delete the file or try installing oscdimg")
+            'Application.Exit()
+        Else
+            MessageBox.Show("File will be downloaded to C:\adk. Approximately 2MB")
+            My.Computer.Network.DownloadFile("https://download.microsoft.com/download/1/f/d/1fd2291e-c0e9-4ae0-beae-fbbe0fe41a5a/adk/adksetup.exe", "c:\adk\adksetup.exe")
+        End If
+    End Sub
+
+    Private Sub Button5_Click_1(sender As Object, e As EventArgs) Handles Button5.Click
+        MessageBox.Show("File will be installed to C:\Program Files (x86)\Windows Kits\10. Download is approximately 96MB")
+        Process.Start("cmd", "/c " + "C:\adk\adksetup.exe /installpath ""C:\Program Files (x86)\Windows Kits\10"" /features OptionId.DeploymentTools""")
+        'C:\adk\adksetup.exe /quiet /installpath "C:\Program Files (x86)\Windows Kits\10" /features OptionId.DeploymentTools
+    End Sub
+End Class
